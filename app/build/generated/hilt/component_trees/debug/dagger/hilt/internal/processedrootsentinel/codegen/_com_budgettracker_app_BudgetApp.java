1b561d85366f3410912e17bdb37a9ece572b0d5e
Sumiraj saroj <sumitrajsaroj01@gmail.com>
2026-08-28 09:38:23 +0530

    Ignore build outputs properly (untrack app/build)

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_budgettracker_app_BudgetApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.budgettracker.app.BudgetApp"
-)
-public final class _com_budgettracker_app_BudgetApp {
-}
--- app/build/generated/ksp/debug/resources/META-INF/proguard/com_budgettracker_app_ui_accounts_AccountsViewModel_HiltModules_KeyModule_LazyClassKeys.pro
+++
@@ -1,1 +0,0 @@
--keep,allowobfuscation,allowshrinking class com.budgettracker.app.ui.accounts.AccountsViewModel
